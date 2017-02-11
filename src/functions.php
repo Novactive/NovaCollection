64efc50103a25fbe20b00f364bae515cce2cac4a
Morel Sébastien <morel.seb@gmail.com>
2017-02-10 18:27:53 -0800

    Add Function constructor

---
+++ src/functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Novactive Collection.
+ *
+ * @author    Luke Visinoni <l.visinoni@novactive.us, luke.visinoni@gmail.com>
+ * @author    Sébastien Morel <s.morel@novactive.us, morel.seb@gmail.com>
+ * @copyright 2017 Novactive
+ * @license   MIT
+ */
+use Novactive\Collection\Factory;
+
+if (!function_exists('Collection')) {
+    function Collection($items)
+    {
+        return Factory::create($items);
+    }
+}
+
+if (!function_exists('NovaCollection')) {
+    function NovaCollection($items)
+    {
+        return Factory::create($items);
+    }
+}
